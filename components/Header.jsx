46b86fb0058d837bcc2dfcac9276215ed99006f0
elouazzani yassine <80113396+yassine121212@users.noreply.github.com>
2022-08-25 13:26:14 +0100

    initi
    inti

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,15 +14,15 @@ const Header = () => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
+      <div className="border-b w-full inline-block border-orange-400 py-4">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Graph CMS</span>
+            <span className="cursor-pointer font-bold text-3xl text-white">Yassine ElDev</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold text-md cursor-pointer">{category.name}</span></Link>
           ))}
         </div>
       </div>
